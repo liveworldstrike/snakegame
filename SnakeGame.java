19bd4aab94bc4697a0ce46119cdeb20c170cdb69
liveworldstrike <lazodellamas12@gmail.com>
2016-04-05 12:04:00 +0200

    añadido clase entera snake y finalizada snake game

--- SnakeGame.java
+++ SnakeGame.java
@@ -7,14 +7,27 @@
  */
 public class SnakeGame
 {
-    
+    private Snake esperma;
+    private Canvas lienzo;
+    private static final int ANCHO_LIENZO = 500;
+    private static final int ALTO_LIENZO = 500;
 
-    /**
-     * Constructor for objects of class SnakeGame
+    /*
+     * Constructor de la clase Snake
      */
     public SnakeGame()
     {
-     
+        esperma = new Snake(ANCHO_LIENZO,ALTO_LIENZO);
+        lienzo = new Canvas("SnakeGame",ANCHO_LIENZO,ALTO_LIENZO);
+    }
+
+    
+    /*
+     * Dibuja una serpiente en la pantalla
+     */
+    public void drawSnake()
+    {
+        esperma.dibujar(lienzo);
     }
 
 }
