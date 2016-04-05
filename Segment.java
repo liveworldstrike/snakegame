a44f7c77f2b47e22e948000c48460dfd0db0620b
liveworldstrike <lazodellamas12@gmail.com>
2016-04-05 09:43:28 +0200

    añadido clases canvas y pen junto a snakegame y segment, segment con sus atributos y constructor ya alñadidos

---
+++ Segment.java
@@ -0,0 +1,41 @@
+import java.awt.Color;
+/**
+ * Write a description of class Segment here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Segment
+{
+    //posicion x
+    private int x;
+    //posicion y 
+    private int y;
+    //posicion final x
+    private int xFinal;
+    //posicion final y
+    private int yFinal;
+    //longitud del segmento 
+    public static final int LONGITUD_SEGMENTO = 4;
+    //color segento 
+    private Color color;
+    //direccion 
+    private int direccion ;
+
+    /**
+     * Constructor for objects of class Segment
+     */
+    public Segment(int x,int y,int direccion ,Color color )
+    {
+        this.x = x;
+        this.y = y;
+        this.xFinal= xFinal;
+        this.yFinal = yFinal;
+    }
+    
+  
+
+   
+    
+    
+}
---
+++ SnakeGame.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class SnakeGame here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SnakeGame
+{
+    
+
+    /**
+     * Constructor for objects of class SnakeGame
+     */
+    public SnakeGame()
+    {
+     
+    }
+
+}
