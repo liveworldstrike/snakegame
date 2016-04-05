e21665a4ae6f42d0b3b68391e7831ea3731c7710
liveworldstrike <lazodellamas12@gmail.com>
2016-04-05 10:23:25 +0200

    modificado contructor clase segment y añadido metodo dibujar

--- Segment.java
+++ Segment.java
@@ -8,13 +8,9 @@ import java.awt.Color;
 public class Segment
 {
     //posicion x
-    private int x;
+    private int posicionX;
     //posicion y 
-    private int y;
-    //posicion final x
-    private int xFinal;
-    //posicion final y
-    private int yFinal;
+    private int posicionY;
     //longitud del segmento 
     public static final int LONGITUD_SEGMENTO = 4;
     //color segento 
@@ -27,14 +23,35 @@ public class Segment
      */
     public Segment(int x,int y,int direccion ,Color color )
     {
-        this.x = x;
-        this.y = y;
-        this.xFinal= xFinal;
-        this.yFinal = yFinal;
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
     }
     
-  
-
+    /**
+     * dibuja la serpiente los 3 segmentos 
+     */
+    public void dibujar(Canvas canvas)
+    {
+      canvas.setForegroundColor(color);
+      //hacia arriba
+      if (direccion == 0){
+          canvas.drawLine(posicionX,posicionY,posicionX,posicionY - LONGITUD_SEGMENTO);
+      }
+      //hacia abajo
+      else if (direccion == 1){
+          canvas.drawLine(posicionX,posicionY,posicionX,posicionY + LONGITUD_SEGMENTO);
+      }
+      //izquierda 
+      else if (direccion == 2){
+         canvas.drawLine(posicionX,posicionY,posicionX- LONGITUD_SEGMENTO,posicionY); 
+      }
+      //derecha 
+      else if (direccion == 3){
+          canvas.drawLine(posicionX,posicionY ,posicionX + LONGITUD_SEGMENTO,posicionY);
+      }
+      
+    }
+    
    
     
     
